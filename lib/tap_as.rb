915794a33e0f6d9867b6264bf6770886f8b80b77
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-10-22 21:40:31 +0900

    define Object#as

--- lib/tap_as.rb
+++ lib/tap_as.rb
@@ -1,5 +1,11 @@
 require "tap_as/version"
 
 module TapAs
-  # Your code goes here...
+  module ObjectExtension
+    def as
+      tap {|object| break yield(object) }
+    end
+  end
 end
+
+Object.__send__ :include, TapAs::ObjectExtension
